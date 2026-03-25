38ef849e6c110eb83e4d99e3d4d9e8c8da8721b2
dhuro523 <dhurimarllati19@gmail.com>
2026-03-25 17:09:43 +0100

    Validate orders

--- Detyra_1.py
+++ Detyra_1.py
@@ -3,3 +3,13 @@ orders = [{"order_id": 1, "price": 50, "quantity": 2},
           {"order_id": 3, "price": 30, "quantity": 0},
           {"order_id": 4, "price": 20, "quantity": 3},
 ]
+
+def validate_orders(orders):
+    valid_orders = []
+    for order in orders:
+        if order["price"] > 0 and order["quantity"] > 0:
+            valid_orders.append(order)
+    return valid_orders
+
+result = validate_orders(orders)
+print(result)
