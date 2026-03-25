170218d04acef42a02d5b2eb8c1aed8c4fafae48
dhuro523 <dhurimarllati19@gmail.com>
2026-03-25 20:36:36 +0100

    Function added, word counter

--- Detyra_4.py
+++ Detyra_4.py
@@ -2,4 +2,11 @@ sentence = "Python is great and python is easy to learn."
 
 words = sentence.lower().split()
 
-print(words)
+word_count = {}
+for word in words:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+
+print(word_count)
