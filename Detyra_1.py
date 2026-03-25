ce0db42575de1a886b73aa9c2a4b664c5ac79a00
dhuro523 <dhurimarllati19@gmail.com>
2026-03-25 16:27:31 +0100

    Add the orders

---
+++ Detyra_1.py
@@ -0,0 +1,5 @@
+orders = [{"order_id": 1, "price": 50, "quantity": 2},
+          {"order_id": 2, "price": -10, "quantity": 1},
+          {"order_id": 3, "price": 30, "quantity": 0},
+          {"order_id": 4, "price": 20, "quantity": 3},
+]
