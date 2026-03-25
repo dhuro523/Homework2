eeb93f0c40230d2389913c44d273f568c6120518
dhuro523 <dhurimarllati19@gmail.com>
2026-03-26 00:13:56 +0100

    Data added

---
+++ detyra_7.py
@@ -0,0 +1,6 @@
+users = [
+    {"id": 1, "name": "John", "salary": 500},
+    {"id": 2, "name": "", "salary": 300},
+    {"id": 3, "name": "Anna", "salary": -100},
+    {"id": None, "name": "Mike", "salary": 400}
+]
