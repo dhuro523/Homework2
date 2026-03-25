b15a136f88a987f653143c91d657cfa4d8465e88
dhuro523 <dhurimarllati19@gmail.com>
2026-03-26 00:18:56 +0100

    Validate needed data

--- detyra_7.py
+++ detyra_7.py
@@ -4,3 +4,20 @@ users = [
     {"id": 3, "name": "Anna", "salary": -100},
     {"id": None, "name": "Mike", "salary": 400}
 ]
+
+invalid_records = []
+
+for user in users:
+    errors = []
+
+    if user["id"] is None:
+        errors.append("ID must not be None.")
+    
+    if user["name"] == "":
+        errors.append("Name must not be Empty.")
+
+    if user["salary"] < 0:
+        errors.append("Salary must be a positive.")
+
+    
+    
